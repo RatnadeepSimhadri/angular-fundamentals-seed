5404262a8e83b0749dc94c6be3ec8a91e14687cf
Ratnadeep Simhadri <ratnadeep.ms@outlook.com>
2018-02-21 00:11:22 -0800

    Property and Event binding

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,42 @@
 import { Component } from '@angular/core';
-
+import  * as Rx from 'rxjs';
+import { fromEvent } from 'rxjs/observable/fromEvent';
 @Component({
   selector: 'app-root',
   styleUrls: ['app.component.scss'],
   template: `
     <div class="app">
-      Hello!
+      <h4>{{title}}</h4>
+      <br>
+      <input 
+      id ="idText"
+      type="text" 
+      (input)="updateData($event)"
+      [value]="text">  
+      <br>
+      <br>
+      <button (click)="handleClick()">Change Text Two</button>
+      <br>
+      <br>
+      <h4>{{texttwo}}</h4> 
     </div>
   `
 })
 export class AppComponent {
+  title: string;
+  text: string;
+  texttwo: string;
+  constructor(){
+    this.title = 'Ultimate Angular';
+    this.text = 'Placeholder Text';
+  }
+
+  updateData($event: any){
+    this.texttwo = $event.target.value;
+  }
+
+  handleClick(){
+    this.texttwo = "Button was Clicked";
+  }
 
 }
